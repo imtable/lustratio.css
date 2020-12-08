93205151400858fbd82d321339506eb537ab2bf1
Zakhar <mrelbat@gmail.com>
2020-12-08 16:54:52 +0200

    asd

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+	require "./assets/less/lessc.inc.php";
+	function autoCompileLess($inputFile, $outputFile) {
+	  // load the cache
+	  $cacheFile = $inputFile.".cache";
+	  if (file_exists($cacheFile)) {
+		$cache = unserialize(file_get_contents($cacheFile));
+	  } else {
+		$cache = $inputFile;
+	  }
+	  $less = new lessc;
+	  $newCache = $less->cachedCompile($cache);
+	  if (!is_array($cache) || $newCache["updated"] > $cache["updated"]) {
+		file_put_contents($cacheFile, serialize($newCache));
+		file_put_contents($outputFile, $newCache['compiled']);
+	  }
+	}
+	autoCompileLess('./assets/less/style.less', './assets/css/style.css');
+// class="col-xs-6 wow fadeIn" data-wow-delay="0.3s" data-wow-duration="0.6s"
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>website</title>
+   <link rel="stylesheet" href="assets/css/lustratio.css">
+   <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+   header.header>.container>nav>ul.menu>li*5.item^^^^main.main>section.section*3>.container^footer.footer
+</body>
+
+</html>
